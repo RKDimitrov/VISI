eafe48caed86a2d401c9ad1ad15423ccb96f98da
RKDimitrov <asrr3fl3x@gmail.com>
2023-03-09 12:30:28 +0200

    Bug

--- updater.py
+++ updater.py
@@ -1,12 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Send a GET request to the Windows 10 update page
 def windows_update():
-    url = 'https://support.microsoft.com/en-us/topic/february-21-2023-kb5022906-os-builds-19042-2673-19044-2673-and-19045-2673-preview-bf72fc27-6222-4f6a-991a-f472b3f9d3fd'
+    url = 'https://support.microsoft.com/en-us/topic/windows-10-update-history-8127c2c6-6edf-4fdf-8b9f-0f7be1ef3562'
     response = requests.get(url)
 
-    # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
 
     windows_update= soup.find('a', {'data-bi-slot': '3'}).text
@@ -17,7 +15,6 @@ def vs_code_update():
     url = 'https://code.visualstudio.com/updates'
     response = requests.get(url)
 
-    # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
 
     active_li = soup.findAll('li', {'class':'active'})
