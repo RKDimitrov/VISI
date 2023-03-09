bf847254aced4264c5c6850d197ee7ef6d3873a6
nikgeo01 <nborgeo@gmail.com>
2023-03-09 18:59:25 +0200

    notifications final version

--- notification.py
+++ notification.py
@@ -1,16 +1,19 @@
 from winotify import Notification
 import interface
 import os, sys
+from updater import AppsToUpdates
+
+APPSTOUPDATE=AppsToUpdates['Windows 10']
 
 def on_notification_clicked(notification):
     print("Notification clicked!")
 
 notification = Notification(
-    app_id="my_app_id",
-    title="Notification Title",
-    msg="Notification Message"
+    app_id="VISI",
+    title="NEW UPDATE AVAILABLE",
+    msg=APPSTOUPDATE
 )
 
-notification.add_actions(label="ghj", launch=f"{os.getcwd()}\interface.py")
+notification.add_actions(label="GO APP", launch=f"{os.getcwd()}\interface.py")
 
 notification.show()
