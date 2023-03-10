47e73c54543bd38185ba25e132ef9ba24480c102
nikgeo01 <nborgeo@gmail.com>
2023-03-10 11:56:44 +0200

    hui.exe

--- modules/app_finder.py
+++ modules/app_finder.py
@@ -1,14 +1,17 @@
-import platform
-
+import os
 # Get the operating system
-os = platform.system()
+
+def search_file_on_windows(filename):
+    for root, dirs, files in os.walk("C:\\"):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
+
+# Example usage
+game_exe_path = search_file_on_windows("hui.exe")
+if game_exe_path is not None:
+    print("Game is installed at:", game_exe_path)
+else:
+    print("Game is not installed.")
 
 # Print different messages based on the OS
-if os == 'Windows':
-    print('You are running Windows')
-elif os == 'Linux':
-    print('You are running Linux')
-elif os == 'Darwin':
-    print('You are running macOS')
-else:
-    print('Unknown operating system')
