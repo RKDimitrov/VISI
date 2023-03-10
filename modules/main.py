a4b938d954b66af48b4dd79ab0ec4222ab30eea5
RKDimitrov <asrr3fl3x@gmail.com>
2023-03-10 23:51:34 +0200

    Flow Tasks partially completed

--- modules/main.py
+++ modules/main.py
@@ -5,11 +5,32 @@ import subprocess
 import threading
 import win32com.client
 from PIL import Image, ImageTk, ImageEnhance
+import time
+import croniter
 
 # Modules
+import VSCodeUpToDater
+import windowsUpToDate
+import ubuntuUpToDater
+import rooster
+
+# Global variables
+update_time = 18000 #30 minutes
+
+def updateCheck():
+    while True:
+        if (VSCodeUpToDater.check_vscode_version()[1] == False) and (windowsUpToDate.check_windows_update()[1] == False) and (ubuntuUpToDater.check_ubuntu_updates()[1] == False):
+            print("No updates needed")
+        else:
+            print("Needs update")
+            subprocess.run(["python", "rooster.py"])
+
+        time.sleep(update_time)
+
 
 def main():
-    pass
+    updateCheck()
+        
 
 if __name__ == "__main__":
     main()
